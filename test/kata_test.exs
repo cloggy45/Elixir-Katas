41e9dff1d4c675f4fdde2e42eff751451e66d352
Paul McLoughlin <paul.mcloughlin@instil.co>
2019-04-21 23:58:22 +0100

    Add test

--- test/kata_test.exs
+++ test/kata_test.exs
@@ -11,4 +11,12 @@ defmodule KataTest do
   test "should add all elements in a list together" do
     assert Kata.sum([1, 5.2, 4, 0, -1]) == 9.2
   end
+
+  test "should convert input to leet speak" do
+    assert Kata.translate("LEET") == "1337"
+    assert Kata.translate("CODEWARS") == "(0D3W@R$"
+    assert Kata.translate("HELLO WORLD") == "#3110 W0R1D"
+    assert Kata.translate("LOREM IPSUM DOLOR SIT AMET") == "10R3M !P$UM D010R $!7 @M37"
+    assert Kata.translate("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG") == "7#3 QU!(K 8R0WN F0X JUMP$ 0V3R 7#3 1@2Y D06"
+  end
 end
