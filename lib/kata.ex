3baed93a5febcb014a34a538152c92ff3d007fff
Paul McLoughlin <paul.mcloughlin@instil.co>
2019-04-22 18:28:24 +0100

    Add rough solution

--- lib/kata.ex
+++ lib/kata.ex
@@ -35,4 +35,57 @@ defmodule Kata do
   def sum(someList) do
     Enum.reduce(someList, fn(cur, acc) -> cur + acc end)
   end
+
+  @spec translate(String.t) :: String.t
+  def translate(someWord) do
+    alphabet = %{
+  A: '@',
+  B: '8',
+  C: '(',
+  D: 'D',
+  E: '3',
+  F: 'F',
+  G: '6',
+  H: '#',
+  I: '!',
+  J: 'J',
+  K: 'K',
+  L: '1',
+  M: 'M',
+  N: 'N',
+  O: '0',
+  P: 'P',
+  Q: 'Q',
+  R: 'R',
+  S: '$',
+  T: '7',
+  U: 'U',
+  V: 'V',
+  W: 'W',
+  X: 'X',
+  Y: 'Y',
+  Z: '2'
+}
+    String.split(someWord, "")
+    |> Enum.map(fn letter ->
+        if letter == "" or letter == " " do
+          letter
+        else
+          String.to_atom letter
+        end
+      end)
+    |> Enum.map(fn atom -> alphabet[atom] end)
+    |> Enum.map(fn element ->
+      if is_nil element do
+        " "
+      else
+        element
+      end
+    end)
+    |> Enum.join
+  end
+
+  def is_not_first_index_or_last(currentIndex, list) do
+    Enum.count(list) - 1 != currentIndex and currentIndex != 0
+  end
 end
